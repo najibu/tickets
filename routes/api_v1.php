eb2237b7d2604aee86a19128d993c315c09b9847
najibu <ns.najibu@gmail.com>
2024-07-27 12:23:12 +0100

    add version

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -3,9 +3,10 @@
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TicketController;
 
-Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
-Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+
+Route::apiResource('tickets', TicketController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
